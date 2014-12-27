835c29f99548e4528861b8446ebf373ba625c48c
rom69 <petitcousin@live.fr>
2014-12-27 17:22:13 +0100

    Ajout de "debug" et tirage d'un nombre aléatoire

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -5,10 +5,18 @@
 ############################
 
 import util #miscellaneous functions utilities
+import random
+
+debug = 0
 
 ## Main program ##
 #Say hi
 util.greeting()
+
+#Randomly pick a number:
+numberToGuess = random.randint(0, 100)
+if(debug):
+    print 'numberToGuess is %d ' % numberToGuess
 
 #Say bye
 util.seeyouing()
