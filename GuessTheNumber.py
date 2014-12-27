dd6667221cad87e0c266d2b55013be8c909b5ae8
rom69 <petitcousin@live.fr>
2014-12-27 17:08:19 +0100

    Création des deux fichiers de bases

---
+++ GuessTheNumber.py
@@ -0,0 +1,14 @@
+############################
+# Game project             #
+# Created the 2014-12-27   #
+#        Main file         #
+############################
+
+import util #miscellaneous functions utilities
+
+## Main program ##
+#Say hi
+util.greeting()
+
+#Say bye
+util.seeyouing()
---
+++ util.py
@@ -0,0 +1,14 @@
+############################
+# Game project             #
+# Created the 2014-12-27   #
+# Utilities functions file #
+############################
+
+#Function which displays the welcoming message:
+def greeting():
+ print "Welcome in the game Guess The Number!"
+
+
+#Function which displays the see-you message:
+def seeyouing():
+ print "'hope you liked the game!"
