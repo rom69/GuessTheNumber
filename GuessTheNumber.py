d2f93270e984d2566fa2a0167003baf7c5a9492a
rom69 <petitcousin@live.fr>
2015-02-28 11:27:36 +0100

    guess the number
    ajout code complet

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -18,5 +18,45 @@ numberToGuess = random.randint(0, 100)
 if(debug):
     print 'numberToGuess is %d ' % numberToGuess
 
+
+from random import randrange
+ 
+def initialize():
+    global ChiffreAleatoire
+    ChiffreAleatoire = randrange(1, 100)
+    print("Entrez un chiffre entre 1 et 100")
+ 
+def VerifieNombre(Chiffre, ChiffreAleatoire):
+    if Chiffre == ChiffreAleatoire:
+        print("C'est le bon chiffre, bravo!")
+        Possible = True
+    elif Chiffre > ChiffreAleatoire:
+        print("C'est moins!")
+        Possible = False
+    elif Chiffre < ChiffreAleatoire:
+        print("C'est plus!")
+        Possible = False
+    return Possible
+ 
+def Continuer():
+    Reponse = input("Voulez vous rejouer (o/n) ? ")
+    if Reponse.upper() == "O":
+        initialize()
+        Boucle = True
+    elif Reponse.upper() == "N":
+        Boucle = False
+    return Boucle
+ 
+Boucle = True
+initialize()
+while Boucle == True:
+    Chiffre = int(input())
+    Possible = VerifieNombre(Chiffre, ChiffreAleatoire)
+    if Possible == True:
+         Boucle = Continuer()
+
+
+
+
 #Say bye
 util.seeyouing()
